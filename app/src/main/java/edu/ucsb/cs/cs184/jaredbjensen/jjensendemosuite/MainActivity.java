8d933153a66866749fda5e5a5c0b73c00dd88b28
Jared Jensen <jared.bradley.jensen@gmail.com>
2017-10-25 21:44:12 -0700

    setup fragment framework

--- app/src/main/java/edu/ucsb/cs/cs184/jaredbjensen/jjensendemosuite/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs184/jaredbjensen/jjensendemosuite/MainActivity.java
@@ -3,6 +3,10 @@ package edu.ucsb.cs.cs184.jaredbjensen.jjensendemosuite;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,24 +17,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import org.w3c.dom.Text;
+
+public class MainActivity extends FragmentActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    static Fragment activeFragment;
+
+    PictureFragment pictureFragment;
+    FireworkFragment fireworkFragment;
+    MovieFragment movieFragment;
+    TextToSpeechFragment textToSpeechFragment;
+    SpeechToTextFragment speechToTextFragment;
+
+    FragmentManager fragmentManager;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +48,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentManager = getSupportFragmentManager();
+
+
     }
 
     @Override
@@ -66,11 +78,6 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -80,22 +87,43 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_picture_viewer) {
+            pictureFragment = new PictureFragment();
+            addFragment(R.id.layout_picture, pictureFragment);
+        } else if (id == R.id.nav_fireworks) {
+            fireworkFragment = new FireworkFragment();
+            addFragment(R.id.layout_firework, fireworkFragment);
+        } else if (id == R.id.nav_movie) {
+            movieFragment = new MovieFragment();
+            addFragment(R.id.layout_movie, movieFragment);
+        } else if (id == R.id.nav_text_to_speech) {
+            textToSpeechFragment = new TextToSpeechFragment();
+            addFragment(R.id.layout_text_to_speech, textToSpeechFragment);
+        } else if (id == R.id.nav_speech_to_text) {
+            speechToTextFragment = new SpeechToTextFragment();
+            addFragment(R.id.layout_speech_to_text, speechToTextFragment);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void addFragment(int layout, Fragment fragment) {
+
+        if (activeFragment == null) {
+            transaction = fragmentManager.beginTransaction();
+            transaction.add(R.id., fragment);
+            transaction.commit();
+            activeFragment = fragment;
+            return;
+        }
+
+        transaction = fragmentManager.beginTransaction();
+        transaction.remove(activeFragment).commit();
+        transaction.replace(layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+        activeFragment = fragment;
     }
 }
